19b5db558fcd3358bca9df7d76547266a2b4fb9c
Naveed Zaheer <nzaheer@microsoft.com>
2021-02-11 14:47:10 -0600

    logging updates

--- DataLoaderFunc/EventDataProcessor.cs
+++ DataLoaderFunc/EventDataProcessor.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Storage.Queues;
+using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
 namespace DataLoaderFunc
 {
@@ -30,6 +32,7 @@ namespace DataLoaderFunc
                     string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
                     string qname = Environment.GetEnvironmentVariable("qname");
                     await CreateQueueAndSendMessage(qname, messageBody, log);
+                    await AddLogRecord(messageBody, log);
                 }
                 catch (Exception e)
                 {
@@ -66,5 +69,56 @@ namespace DataLoaderFunc
             }
             return;
         }
+
+        private static async Task AddLogRecord(string messageBody, ILogger iLog)
+        {
+            var jArray = JArray.Parse(messageBody);
+            string subject = jArray[0]["subject"].Value<string>();
+            string fileName = subject.Substring(subject.LastIndexOf("/") + 1);
+
+            var connectionString = Environment.GetEnvironmentVariable("qstorageconn");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration()); var cloudTableClient = storageAccount.CreateCloudTableClient();
+            var loggingTable = cloudTableClient.GetTableReference("datarecords");
+            await loggingTable.CreateIfNotExistsAsync();
+            var log = new LogEntity(fileName);
+            var insertOp = TableOperation.Insert(log);
+            await loggingTable.ExecuteAsync(insertOp);
+        }
     }
+
+    public class LogEntity : TableEntity
+    {
+        public LogEntity(string fileName)
+        {
+            this.PartitionKey = fileName;
+            this.RowKey = Guid.NewGuid().ToString("N");
+        }
+    }
+
+    //public class LogEntity : Microsoft.WindowsAzure.Storage.Table.ITableEntity
+    //{
+    //    public LogEntity(string fileName)
+    //    {
+    //        this.PartitionKey = fileName;
+    //        this.RowKey = Guid.NewGuid().ToString("N");
+    //        this.Timestamp = DateTimeOffset.UtcNow;
+    //        this.ETag = this.RowKey;
+    //    }
+
+    //    public string PartitionKey { get; set; }
+    //    public string RowKey { get; set ; }
+    //    public DateTimeOffset Timestamp { get; set; }
+    //    public string ETag { get; set; }
+
+    //    public void ReadEntity(IDictionary<string, Microsoft.WindowsAzure.Storage.Table.EntityProperty> properties, Microsoft.WindowsAzure.Storage.OperationContext operationContext)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public IDictionary<string, Microsoft.WindowsAzure.Storage.Table.EntityProperty> WriteEntity(Microsoft.WindowsAzure.Storage.OperationContext operationContext)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
